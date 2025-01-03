0ec91a9b19d634d5f32e47ffaeb806943c666c08
Dylan <109418123+WebbD777@users.noreply.github.com>
2025-01-03 17:17:13 +0200

    The Output page is now formatted nicely

--- outPutPage.xaml.cs
+++ outPutPage.xaml.cs
@@ -4,28 +4,39 @@ namespace tresuaryAPi2;
 
 using Microsoft.Maui.Devices;
 using Newtonsoft.Json.Linq;
+using System.Collections.ObjectModel;
 using System.Net.Http;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Text.Json;
+using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 public partial class outPutPage : ContentPage
 {
     List<string> sum = new List<string>();
     List<string> functionLabel = new List<string>();
     List<string> outPutLs = new List<string>();
+    string _label;
+
+    MainViewModel model = new MainViewModel();
     public outPutPage()
 	{
 		InitializeComponent();
-	}
+        
+    }
 
     public outPutPage(int year, string itemCode, string label, string demarCode)
     {
         InitializeComponent();
+        _label = label;
 
         //outPutLabel.Text = year + " " + itemCode + " " + label + " " + demarCode;
         string url = $"https://municipaldata.treasury.gov.za/api/cubes/incexp/aggregate?drilldown=function.label|item.return_form_structure&cut=financial_year_end.year:{year}|amount_type.code:AUDA|financial_period.period:{year}|demarcation.code:\"{demarCode}\"|item.code:\"{itemCode}\"&aggregates=amount.sum";
         getOutput(url);
+
+        outPutLabel.Text = _label;
+
+        
     }
 
     private async Task<List<List<string>>> getOutput(string url)
@@ -47,7 +58,7 @@ public partial class outPutPage : ContentPage
 
                 if (data["cells"] != null)
                 {
-                    outPutLabel.Text = "data not null";
+                  //  outPutLabel.Text = "data not null";
                     int count = 0;
 
                     foreach (var item in data["cells"])
@@ -55,15 +66,17 @@ public partial class outPutPage : ContentPage
                         if (item["function.label"] != null)
                         {
                             string sumStr = item["amount.sum"].ToString().Split(',')[0];
-                            string functionStr =item["function.label"].ToString().Split(',')[0];
+                            string functionStr = item["function.label"].ToString().Split(',')[0];
 
                             count++;
                             // displayInfo.Text = itemCode;
 
-                            string outPutStr = functionStr + "\nTotal amount: R" + sumStr;
+                          /*  string outPutStr = functionStr + "\nTotal amount: R" + sumStr + "\n\n";
                             outPutLs.Add(outPutStr);
-                            // sum.Add(sumStr);
-                            // functionLabel.Add(functionStr);
+                            sum.Add(sumStr);
+                            functionLabel.Add(functionStr);*/
+
+                            model.MuniItems.Add(new MuniItem { LabelText = functionStr, AmountText = "R"+sumStr });
                         }
                     }
                 }
@@ -72,7 +85,9 @@ public partial class outPutPage : ContentPage
                     //outPutLabel.Text = "data null";
                 }
 
-                lsView.ItemsSource = outPutLs;
+                //MuniList.ItemsSource = outPutLs;
+
+                BindingContext = model;
 
                 //  outPutLabel.Text = "Did soemthing";
                 /*List<String> arrears = new List<string>();
@@ -84,10 +99,29 @@ public partial class outPutPage : ContentPage
                 labels.Add(names);*/
             }
             else {
-                outPutLabel.Text = "Unsuccessful response";
+             //   outPutLabel.Text = "Unsuccessful response";
             }
         }
 
         return labels;
     }
+}
+
+class MuniItem
+{
+    public string LabelText { get; set; }
+    public string AmountText { get; set; }
+}
+
+class MainViewModel
+{
+    public ObservableCollection<MuniItem> MuniItems { get; set; }
+
+    public MainViewModel()
+    {
+        // Initialize with some sample data
+        MuniItems = new ObservableCollection<MuniItem>();
+    }
+
+    
 }
